3f2be099a64deff47f162402b8eb4be549cd84eb
Nikan <njavadzadeh@gmail.com>
2022-12-17 20:23:03 -0800

    commit tree cfc630378555ff389b165ee8f51e1ba5e59354de

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,33 @@
 require_relative './stack'
+require 'pry'
 
-# your code here
+def balancing_parentheses(string)
+
+stack =Stack.new
+string.each_char do |char|
+if char == '(' 
+ stack.push(char)
+elsif char == ')'
+   if stack.empty?
+      stack.push('(')
+   else
+    stack.pop
+     end
+
+    end
+
+   end
+
+    stack.size
+    
+end
+
+
+     
+
+    
+
+
+
+
+
